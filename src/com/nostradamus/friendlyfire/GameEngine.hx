a862f620645ed0af9069b41adb78a6db5a1698ee
Shuheng <huangshu91@gmail.com>
2012-11-21 22:32:11 -0500

    changed the entry class to GameEngine and added a Config class to store statics/constants

---
+++ src/com/nostradamus/friendlyfire/GameEngine.hx
@@ -0,0 +1,39 @@
+package com.nostradamus.friendlyfire;
+
+import com.haxepunk.Engine;
+import com.haxepunk.HXP;
+
+import com.nostradamus.util.Config;
+
+/**
+ * ...
+ * @author maiev
+ */
+
+class GameEngine extends Engine 
+{
+	function new()
+	{
+		super(Config.kScreenWidth, Config.kScreenHeight, Config.kFrameRate, false);	
+	}
+	
+	override public function init()
+	{
+#if debug
+	#if flash
+		if (flash.system.Capabilities.isDebugger)
+	#end
+		{
+			HXP.console.enable();
+		}
+#end
+		HXP.screen.color = Config.kClearColor;
+		HXP.screen.scale = 1;
+//		HXP.world = new YourWorld();
+	}
+
+	public static function main()
+	{
+		var app = new GameEngine();
+	}
+}
---
+++ src/com/nostradamus/util/Config.hx
@@ -0,0 +1,20 @@
+package com.nostradamus.util;
+
+/**
+ * ...
+ * @author maiev
+ */
+
+class Config 
+{
+  /*
+   * (Maiev):
+   * Need to scale this for mobile versions.
+   */
+  public static inline var kScreenWidth:Int = 640;
+	public static inline var kScreenHeight:Int = 480;
+  
+	public static inline var kFrameRate:Int = 60;
+	public static inline var kClearColor:Int = 0x333333;
+  public static inline var kProjectName:String = "FriendlyFire";
+}
