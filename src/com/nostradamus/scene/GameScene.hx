4fc3f77076651b5b3225d4fd8c4630a384cdb7eb
huangshu <huangshu@gmail.com>
2012-11-22 13:52:30 -0500

    pulling merge

--- src/com/nostradamus/scene/GameScene.hx
+++ src/com/nostradamus/scene/GameScene.hx
@@ -2,16 +2,24 @@ package com.nostradamus.scene;
 
 import com.haxepunk.World;
 import com.haxepunk.HXP;
+import com.nostradamus.entity.PlayerEntity;
+
+import com.nostradamus.entity.EntitySys;
 
 /**
  * ...
  * @author maiev
  */
 
+/*
+ * (maiev): 
+ * For now this can will be the "game" and we put all logic here.
+ * In the future, decide what map to load, add networking code, etc
+ */
 class GameScene extends World
 {
-
   private static var bgColor:Int = 0x63524f;
+  private var entityManager:EntitySys;
   
   public function new() 
   {
@@ -21,6 +29,14 @@ class GameScene extends World
   public override function begin()
   {
     HXP.screen.color = bgColor;
+    
+    //hack the values for now.
+    add(new PlayerEntity(HXP.halfWidth - 16, HXP.halfHeight - 16));
   }
   
+  public override function update() 
+  {
+    
+    
+  }
 }
